51c50b3d201e6d3db6442bc6a9af44479e12d0e8
Mohamed Rezk <mrezk880@gmail.com>
2026-03-06 17:41:29 +0200

    add load user and checking user existence and part of saving the question implementation

--- src/storage/FileStore.cpp
+++ src/storage/FileStore.cpp
@@ -40,11 +40,79 @@ bool FileStore::saveUser_t(const User_t &u)
 
 optional<User_t> FileStore::loadUser_t(const string &User_tname)
 {
+    fs::path fullpath = fs::path(this->data_dir) / this->user_dir;
+    fullpath = fullpath / (User_tname + ".txt");
+    if(!fs::exists(fullpath))
+    {
+        return std::nullopt;
+    }
+    ifstream(inFile);
+    if (!inFile.is_open())
+    {
+        std::cerr << "Failed to open " << fullpath << " for reading" << endl;
+        return std::nullopt;
+    }
+    User_t user_data;
+    string line;
+    while (getline(inFile, line))
+    {
+        auto pos = line.find('=');
+        if (pos == string::npos)
+        {
+            continue; // no = found skip
+        }
+
+        string key = line.substr(0, pos);
+        string value = line.substr(pos + 1);
+        if (key == "username")
+            user_data.username = value;
+        else if (key == "password_hash")
+            user_data.password_hash = value;
+        else if (key == "email")
+            user_data.email = value;
+        else if (key == "bio")
+            user_data.bio = value;
+        else if (key == "allow_anonymous")
+            user_data.allow_anonymous = (value == "1");
+        else if (key == "created_at")
+            user_data.created_at = stol(value);
+    }
+
+    return user_data;
 }
 bool FileStore::User_tExists(const string &User_tname)
 {
+        fs::path fullpath = fs::path(this->data_dir) / this->user_dir;
+        fullpath=fullpath/(User_tname+".txt");
+        return fs::exists(fullpath);
+
 }
 
 string FileStore::saveQuestion(const Question_t &q)
 {
+        fs::path fullpath = fs::path(this->data_dir) / this->user_dir;
+    if (!fs::create_directories(fullpath) && !fs::exists(fullpath))
+    {
+        cerr << "Failed to create directory: " << fullpath << "\n";
+        return "";
+    }
+    fullpath = fullpath / ("q_"+q.id + ".txt");
+    FileLock fLock(fullpath);
+
+    if (!fLock.is_locked())
+    {
+        return "";
+    }
+
+    ofstream outfile(fullpath);
+    if (!outfile.is_open())
+    {
+        cerr << "failed to open " << fullpath << " for writing" << endl;
+        return "";
+    }
+
+    outfile<<"id="<<q.id<<"\n";
+  
+
+
 }
